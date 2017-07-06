3730057e6b710bab1f7376ee7bf3ec67fe076a8b
Aurélien Lambert <aurelien.lambert@radiolineapp.com>
2017-07-06 17:03:16 +0200

    far more clever "as" and "track by" expressions comparison, and many tests

--- src/uisRepeatParserService.js
+++ src/uisRepeatParserService.js
@@ -60,21 +60,91 @@ uis.service('uisRepeatParser', ['uiSelectMinErr','$parse', function(uiSelectMinE
 
     // Try to compute modelToTrackByMapper, such that 
     // modelToTrackByMapper on modelMapper is equivalent to trackByMapper
-    // It makes sense when match[1] is a prefix of match[6]
-    var itemName = match[4] || match[2],
-        modelToTrackByMapper;
-    if(!match[1] || !match[6]){
-      modelToTrackByMapper = null;
-    }else if(match[1] === match[6]){
-      modelToTrackByMapper = $parse(itemName);
-    }else if(match[6].substring(0, match[1].length + 1) === match[1] + '.'){
-      try{
-        modelToTrackByMapper = $parse(itemName + '.' + match[6].substring(match[1].length + 1));
-      }catch(e){
+    // Only in the case where the "as" expression is used in the "track by" exrepssion
+    var itemName = match[4] || match[2], 
+        trackByMapper = null, 
         modelToTrackByMapper = null;
+    if(match[6]){
+      // returns all occurences of expr in str as a variable
+      var exprIsUsed = function(str, expr){
+        // look for occurences of expr in str
+        var positions = [], 
+            i = 0;
+        while(true){
+          i = str.indexOf(expr, i);
+          if(i < 0) break;
+          positions.push([i, i + expr.length]);
+          i += expr.length;
+        }
+        if(!positions.length) return positions;
+        // Parse str to look for real occurences of expr as variable
+        var backslash = false, 
+            string = null, 
+            previous = '', 
+            found = [];
+        var strstart = /['"]/, 
+            space = /\s/, 
+            letter = /[a-z]/, 
+            noprev = /[\w$]|\s*\./, 
+            varchar = /[\w$]/;
+        for(i = 0; i < str.length; i++){
+          if(positions[0][0] == i){
+            // expr is escaped, part of a string or inside a longer path
+            if(backslash || string || noprev.test(previous)){
+              positions.shift();
+              if(!positions.length) return found;
+            }
+          }else if(positions[0][1] == i){
+            // check that the next character is not a variable character
+            if(!varchar.test(str[i])){
+              found.push(positions[0]);
+            }
+            positions.shift();
+            if(!positions.length) return found;
+          }
+          if(backslash) backslash = false; // this character is escaped
+          else if(str[i] === '\\') backslash = true; // next character is escaped
+          else if(string){ // inside a string
+            if(str[i] === string) string = null; // last character of this string
+          }else if(strstart.test(str[i])){ // first character of a string
+            string = str[i];
+            previous = '';
+          }else if(space.test(str[i])){ // a space, can be end of an operator like "in" or "typeof"
+            if(letter.test(previous)) previous = '';
+          }else previous = str[i]; // another character
+        }
+        // expr is at the end
+        found.push(positions[0]);
+        return found;
+      };
+      // if $index is used in "track by" expression, it cannot be used for matching
+      if(!exprIsUsed(match[6], '$index').length){
+        trackByMapper = $parse(match[6]);
+        // if $index is used in "as" expression, it cannot be used for matching
+        if(match[1] && !exprIsUsed(match[1], '$index').length){
+          // Check that every occurrence of itemName in the "track by" expresison is inside a copy of the "as" expression
+          // and replace every occurence of the "as" expression in the "track by" by itemName
+          var itempos = exprIsUsed(match[6], itemName);
+          var modelpos = exprIsUsed(match[6], match[1]);
+          var parts = [], 
+              index = 0;
+          while(modelpos.length){
+            // this occurence of itemName is inside this copy of "as" expression
+            if(itempos.length && itempos[0][0] >= modelpos[0][0] && itempos[0][1] <= modelpos[0][1]){
+              itempos.shift();
+            }else{ // there is not other occurence of itemName inside this copy of "as" expression
+              parts.push(match[6].substring(index, modelpos[0][0]), itemName);
+              index = modelpos[0][1];
+              modelpos.shift();
+            }
+          }
+          parts.push(match[6].substring(index, match[6].length));
+          if(!itempos.length){ // all occurences of itemName were inside a copy of the "as" expression
+            try{ modelToTrackByMapper = $parse(parts.join(''));
+            }catch(e){} // ignore errors of parsing in absurdly complex case
+          }
+        }
       }
-    }else{
-      modelToTrackByMapper = null;
     }
 
     return {
@@ -83,7 +153,7 @@ uis.service('uisRepeatParser', ['uiSelectMinErr','$parse', function(uiSelectMinE
       source: $parse(source),
       filters: filters,
       trackByExp: match[6],
-      trackByMapper: match[6] && $parse(match[6]),
+      trackByMapper: trackByMapper,
       modelToTrackByMapper: modelToTrackByMapper,
       modelMapper: $parse(match[1] || itemName),
       repeatExpression: function (grouped) {
